f9396d86437d1231bd5fbab1794c10aca460960f
Omar Roth <omarroth@hotmail.com>
2017-12-30 15:46:32 -0600

    Squashed 14 commits from December 2017
    - Add fix for args order and rename videos table
    - Add sentry (from updated fork)
    - Add fontawesome
    - Add search
    - Remove jquery
    - Update layout.ecr, replace navbar and add functional search
    - Adds description, video class, wilson score...
    - Adds description, video class, wilson score...
    - Add error page and give video.ecr more detailed video info
    - Remove unused info
    - Run 'crystal tool format'
    - Update usage instructions
    - Update postgres script
    - Fix usage instructions

--- libertube.sql
+++
@@ -1,1 +0,0 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/42c47dd273121e815b86420f0af025fb
--- src/libertube.cr
+++
@@ -1,1 +0,0 @@
-/var/folders/15/5nqgf_n51czb2vfntylx44tw4mppxx/T/repo_cache/15709fef8eb6259f3fead3e7448e8bdf
